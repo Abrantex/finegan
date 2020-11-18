ba7d8e2475339fe9ed6963d8dbf263495db975b6
Matheus Cerqueira <matheus.abrantesc@gmail.com>
2020-11-18 10:19:19 -0400

    fix: fix the print of losses
    Signed-off-by: Matheus Cerqueira <matheus.abrantesc@gmail.com>

--- code/trainer.py
+++ code/trainer.py
@@ -494,12 +494,16 @@ class FineGAN_trainer(object):
 
             end_t = time.time()
             
-            #print('''[%d/%d][%d]
-            #             Loss_D: %.2f Loss_G: %.2f Time: %.2fs
-            #          '''
-            #      % (epoch, self.max_epoch, self.num_batches,
-            #         errD_total.data[0], errG_total.data[0],
-            #         end_t - start_t))
+            #print("err type {} {} ".format(type(errD_total), type(errD_total)))
+
+            #print("err {}  {}".format(errD_total.item(), errD_total.item()))
+
+            print('''[%d/%d][%d]
+                         Loss_D: %.2f Loss_G: %.2f Time: %.2fs
+                      '''
+                  % (epoch, self.max_epoch, self.num_batches,
+                     errD_total.item(), errG_total.item(),
+                     end_t - start_t))
 
         save_model(self.netG, avg_param_G, self.netsD, count, self.model_dir)
 
@@ -617,13 +621,13 @@ class FineGAN_trainer(object):
             if(step%50 ==0):
                     print("finishing step", step)
 
-            #if (count % 100) == 0:
-                #print('''[%d/%d][%d]
-                #             Loss_D: %.2f Loss_G: %.2f Time: %.2fs
-                #          '''
-                #      % (count, cfg.TRAIN.HARDNEG_MAX_ITER, self.num_batches,
-                #         errD_total.data[0], errG_total.data[0],
-                #         end_t - start_t))
+            if (count % 100) == 0:
+                print('''[%d/%d][%d]
+                             Loss_D: %.2f Loss_G: %.2f Time: %.2fs
+                          '''
+                      % (count, cfg.TRAIN.HARDNEG_MAX_ITER, self.num_batches,
+                         errD_total.item(), errG_total.item(),
+                         end_t - start_t))
 
             if (count == cfg.TRAIN.HARDNEG_MAX_ITER):
                 # Hard negative training complete
